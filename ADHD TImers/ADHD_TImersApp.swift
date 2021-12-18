b709c13bd37dd0815be96e7d9d2b1bf080f349c9
Daniel Pressner <daniel.pressner@gmail.com>
2021-12-18 09:11:46 +0100

    Initial Commit

---
+++ ADHD TImers/ADHD_TImersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ADHD_TImersApp.swift
+//  ADHD TImers
+//
+//  Created by Daniel Pressner on 18.12.21.
+//
+
+import SwiftUI
+
+@main
+struct ADHD_TImersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
